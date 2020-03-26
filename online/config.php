fc070489281cd0b3e80e913b78fb08d3462d0123
PizzanTrop <pizzantrop@gmail.com>
2020-03-26 16:47:20 +0300

    0.1
    ver 0.1

---
+++ online/config.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Автор: Spark108 | Rubukkit: Sparksys
+ * Mail: spark.vat@gmail.com
+ * Назначение файла: Конфигурация мониторинга.
+*/
+
+return array(
+
+    'href' => 'http://ugw.su/',
+    'display_offline' => false,
+    'symbol_offline' => '&infin;',
+    'cache_monitoring' => true,
+    'cache_update' => 30
+
+);
---
+++ online/servers.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * Автор: Spark108 | Rubukkit: Sparksys
+ * Mail: spark.vat@gmail.com
+ * Назначение файла: Список серверов.
+ * Дополнительно: Сервера указываются в формате ['Имя сервера (Можно с HTML ссылкой)' => 'IP:PORT'].
+*/
+
+return array(
+
+    array('name' => 'Онлайн :', 'address' => 'EU.MINEPLEX.COM:25565'), 
+
+);
